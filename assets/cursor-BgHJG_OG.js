9028d1d2174447dae774db56041ae67e2a2ad539
iunlockexpert <iunlockexpert@yahoo.com>
2025-05-17 22:31:20 +0300

    Updates

--- assets/cursor-BgHJG_OG.js
+++
@@ -1,3 +0,0 @@
-import{V as o}from"./CheckoutPage-DHvYgT8M.js";import"./index-DtOEqnae.js";import"./invoiceUtils-BAW6GaxJ.js";import"./shield-check-BgWLhifv.js";import"./credit-card-C3iOj_O5.js";import"./file-text-DnBqktik.js";const s=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{s as cursorSvg};
---
+++ assets/external-link-R4vGfkwX.js
@@ -0,0 +1,8 @@
+import{V as o}from"./CheckoutPage-mc3wMraJ.js";import"./index-CrRTFIpj.js";import"./invoiceUtils-BZhIwNmf.js";import"./shield-check-KtEJZeJP.js";import"./credit-card-urkM2UVK.js";import"./file-text-Coe2MRqq.js";const a=o`<svg fill="none" viewBox="0 0 14 15">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M3.74 3.99a1 1 0 0 1 1-1H11a1 1 0 0 1 1 1v6.26a1 1 0 0 1-2 0V6.4l-6.3 6.3a1 1 0 0 1-1.4-1.42l6.29-6.3H4.74a1 1 0 0 1-1-1Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{a as externalLinkSvg};
